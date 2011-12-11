a2609bb5a2239fd16d62d3e1ba1d9b0b81277d76
alfwed <alfwed@alfwed.com>
2011-12-11 19:14:01 +0100

    add autoload for unit tests

--- tests/KifsPhpUnit.php
+++ tests/KifsPhpUnit.php
@@ -2,3 +2,7 @@
 
 set_include_path(realpath(__DIR__.'/../vendors').PATH_SEPARATOR.
 		get_include_path());
+
+spl_autoload_register(function($className) {
+    require str_replace('\\', '/', $className).'.php';
+});
