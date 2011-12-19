9ec71cb553cdae25f6013afe84fb67cadafdf6f4
aaltun <aaltun@ellemnrv>
2011-12-19 20:40:16 +0100

    add unit test for Url helper

---
+++ tests/Kifs/View/Helper/UrlTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use \Kifs\Controller\Router\Route;
+
+class UrlTest extends PHPUnit_Framework_TestCase
+{
+	private $_url;
+
+
+	public function setUp()
+	{
+		$rootUrl = 'http://localhost/';
+		$conf = array(
+			new Route('/foo', 'Bar', array()),
+			new Route('/foo/:num', 'Baz', array(':num' => 'int')),
+		);
+
+		$this->_url = new \Kifs\View\Helper\Url($conf, $conf);
+	}
+
+	public function testGetUrlRoot()
+	{
+		$this->assertEquals('http://localhost', $this->_url->getUrl(''));
+	}
+
+	public function testGetUrlForDefaultRoute()
+	{
+		$this->assertEquals('http://localhost/default', $this->_url->getUrl('default'));
+	}
+
+	public function testGetUrlForDefaultRouteWithSubDirectory()
+	{
+		$this->assertEquals('http://localhost/dir/default', $this->_url->getUrl('dir\default'));
+	}
+
+	public function testGetUrlForDefaultRouteWithSubDirectory()
+	{
+		$this->assertEquals('http://localhost/dir/default', $this->_url->getUrl('dir\default'));
+	}
+
+	public function testGetUrlForCustomRoute()
+	{
+		$this->assertEquals('http://localhost/bar', $this->_url->getUrl('foo'));
+	}
+
+	public function testGetUrlForCustomRouteWithParameters()
+	{
+		$this->assertEquals('http://localhost/foo/2', $this->_url->getUrl('baz', array(':num' => 2)));
+	}
+
+}
